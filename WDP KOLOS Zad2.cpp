390a8becc1b9c3826e16c2e44fd7f35323e1c1fa
Igor Rurkowski <93292404+TheNoxy@users.noreply.github.com>
2023-01-16 20:55:54 +0100

    Update WDP KOLOS Zad2.cpp

--- WDP KOLOS Zad2.cpp
+++ WDP KOLOS Zad2.cpp
@@ -1,10 +1,5 @@
-// WDP KOLOS Zad2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
 using namespace std;
-
 int sumowanie(int n)
 {
 	int suma2 = 0;
@@ -16,9 +11,7 @@ int sumowanie(int n)
 			suma = 6 * i;
 			suma2 += 6 * i;
 			cout << suma << ",";
-			
 		}
-		
 	}
 	cout << endl;
 	return suma2;
@@ -26,21 +19,7 @@ int sumowanie(int n)
 
 int main()
 {
-	
 	int n;
 	cin >> n;
 	cout << sumowanie(n);
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-// 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
