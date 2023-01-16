e117e1af7f3fc1010f6a73f9c2c0b043f6a9cad7
Igor Rurkowski <93292404+TheNoxy@users.noreply.github.com>
2023-01-16 15:20:43 +0100

    Add files via upload

---
+++ Kolos wdp_poniedziale ZAD5.cpp
@@ -0,0 +1,42 @@
+// Kolokwium_poniedzialek zad3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void piramida(int w, int a)
+{
+	int b = 0;
+	for (int i = 1; i <= w; i++)
+	{
+		a *= 2;
+		b = a;
+		for (int j = 1; j <= i; j++)
+		{
+			cout << b << " ";
+			b += a;
+		}
+		cout << endl;
+	}
+
+}
+
+int main()
+{
+	int w;
+	cin >> w;
+	int a = 1;
+	piramida(w, a);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
